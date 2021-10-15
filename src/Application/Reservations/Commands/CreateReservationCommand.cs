13f6908ca926f14c2aa0bc78bb07f2957e4f795c
MartinManolov <mmmanolov@gmail.com>
2021-10-15 17:27:46 +0300

    Added CreateReservationCommand

---
+++ src/Application/Reservations/Commands/CreateReservationCommand.cs
@@ -0,0 +1,39 @@
+﻿namespace Accommodation.Application.Reservations.Commands
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Accommodation.Application.Common.Interfaces;
+    using Accommodation.Domain.Entities;
+    using MediatR;
+
+    public record CreateReservationCommand (string HotelId, string OfferId) : IRequest<string>
+    {
+    }
+
+    public class CreateReservationCommandHandler : IRequestHandler<CreateReservationCommand, string>
+    {
+        private readonly IApplicationDbContext _context;
+        private readonly ICurrentUserService _userService;
+
+        public CreateReservationCommandHandler(IApplicationDbContext context, ICurrentUserService userService)
+        {
+            _context = context;
+            _userService = userService;
+        }
+
+        public async Task<string> Handle(CreateReservationCommand request, CancellationToken cancellationToken)
+        {
+            var reservation = new Reservation
+            {
+                GuestId = _userService.UserId,
+                HotelId = request.HotelId,
+                OfferId = request.OfferId,
+            };
+
+            await _context.Reservations.AddAsync(reservation);
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return reservation.Id;
+        }
+    }
+}
